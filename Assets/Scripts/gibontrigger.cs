9df4a26bf1898bf6aa66b2fd9efdd0c9bc134b6b
TryharderFr <admin@tryharder.fr>
2017-02-06 07:42:50 +0100

    no message

---
+++ Assets/Scripts/gibontrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class gibontrigger : MonoBehaviour
+{
+    public GameObject gib;
+
+    void OnTriggerEnter()
+    {
+        Instantiate(gib, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/moveleftandright.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class moveleftandright : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += Vector3.right * Input.GetAxis("Horizontal") *speed* Time.deltaTime;
+	}
+}
